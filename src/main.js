9b861f791e63cd1deafebb58b1f4beb994ece9c5
Daria Grigoriu <dariagrigoriu@Darias-MBP.home>
2019-04-21 22:28:47 -0700

    coffee demo client

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import "bootstrap-vue/dist/bootstrap-vue.css"
 import VueWordCloud from 'vuewordcloud';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueRouter from 'vue-router'
 
 Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios);
+Vue.use(VueRouter)
 
 Vue.component(VueWordCloud.name, VueWordCloud);
 
@@ -22,7 +24,37 @@ Vue.component('news-item', {
   props: ['title']
 })
 
+const locations = { template: '<div class="tab-pane active">locations tab content...</div>' }
+const blends = { template:'<div class="tab-pane active">blends tab.. </div>' }
+const art  = { template:'<div class="tab-pane active">art tab..</div>' }
+const about  = { template:'<div class="tab-pane active">about tab..</div>' }
+const menu  = { template:'<div class="tab-pane active">\
+  <b-img src="./static/coffee_zoka_2.jpg" fluid-grow alt="Coffee life image"/>\
+</div>'}
+
+Vue.component('menu-order-item', menu);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/menu', name:'menu', component: menu, alias: '*' },
+    { path: '/locations', name:'locations', component: locations },
+    { path: '/blends', name:'blends', component: blends },
+    { path: '/art', name:'art', component: art },
+    { path: '/about', name:'about', component: about },
+  ]
+})
+
+/*
+ <b-nav-item active>Menu</b-nav-item>
+    <b-nav-item>Location</b-nav-item>
+    <b-nav-item>Blends</b-nav-item>
+    <b-nav-item disabled>Art</b-nav-item>
+    <b-nav-item >About</b-nav-item>
+*/
+
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
